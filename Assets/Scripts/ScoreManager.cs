5e990af8f36d46e9d00733af0d7e0889cb9b0824
Sanches Mateus Alexandre <alexandre.sanches_mateus@edu.devinci.fr>
2021-10-22 17:07:51 +0200

    Ajout script score + canvas

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text scoreUI;
+    public float speed = 1;
+    public float currentScore = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentScore += Time.deltaTime * speed;
+        scoreUI.text = "" + (int)currentScore;
+    }
+}
